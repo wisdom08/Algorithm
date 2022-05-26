604a5f3c83d4d376f55bdab582102a8258c3fb78
wisdom <wisdom08@kakao.com>
2022-05-27 00:08:44 +0900

    2022-05-26 추가

---
+++ algorithm/leetcode/Running Sum of 1d Array.js
@@ -0,0 +1,10 @@
+const runningSum = (nums) => {
+    let arr = [];
+    let sum = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i];
+        arr.push(sum);
+    }
+    return arr;
+}

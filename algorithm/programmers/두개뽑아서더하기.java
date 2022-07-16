ba136d349c404925852a2a86c7fa3bd9371c388f
wisdom <wisdom08@kakao.com>
2022-07-16 13:39:56 +0900

    2022-07-16 추가

---
+++ algorithm/programmers/두개뽑아서더하기.java
@@ -0,0 +1,71 @@
+/*
+ * https://school.programmers.co.kr/learn/courses/30/lessons/68644
+ * 문제 분석
+ * 정수 배열에서 서로 다른 인덱스에 있는 두 개의 수를 뽑아 더해서 만들 수 있는 모든 수의 배열을 오름차순으로 담아 리턴한다.
+ *
+ * 계획
+ * 이중 for 문
+ * 1. 이중 반복문 안에서 하나 씩, 하나 씩 전부 더한다.
+ * 2. 새로 만든 배열에 위에서 더한 값이 있는지 contains() 함수로 체크하고 없으면 배열에 추가한다.
+ * 3. 오름차순으로 정렬하고 리턴한다.
+ *
+ * HashSet
+ * 1. 종복을 허용하지 않는 HashSet 저장할 공간 만들기
+ * 2. 각각의 합을 리털한 배열 공간에 추가하고 정렬한다.
+ * */
+
+package programmers;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+
+public class 두개뽑아서더하기 {
+    public static void main(String[] args) {
+        Solution04 p = new Solution04();
+
+        int[] numbers = {2, 1, 3, 4, 1};
+        p.solution(numbers);
+        p.solution_(numbers);
+    }
+}
+
+
+class Solution04 {
+    public int[] solution(int[] numbers) {
+        ArrayList<Integer> list = new ArrayList<>();
+        int sum;
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                sum = numbers[i] + numbers[j];
+                if (!list.contains(sum)) {
+                    list.add(sum);
+                }
+            }
+        }
+
+        int[] answer = new int[list.size()];
+        Collections.sort(list);
+
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = list.get(i);
+        }
+
+        return answer;
+    }
+
+    public int[] solution_(int[] numbers) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                set.add(numbers[i] + numbers[j]);
+            }
+        }
+        return set.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
+/*
+ * 회고
+ * 스트림이 익숙하지 않은데 '데이터가 흐르는 통로' 로 일단 이해하자.
+ * 그리고 스트림을 잘 활용하면 함수형 프로그래밍도 가능하고 코드도 더 간결하게 만들어줘서 장점이 많아보인다.
+ * */
---
+++ algorithm/programmers/하샤드수.java
@@ -0,0 +1,39 @@
+/*
+ * https://school.programmers.co.kr/learn/courses/30/lessons/12947
+ * 문제 분석
+ * 하샤드수의 정의: 주어진 양의 정수 x의 자릿수의 합으로 x가 나누어져야 한다.
+ * 하샤드수가 맞다면 true / 아니라면 false 반환
+ *
+ * 계획
+ * 1. x의 자릿수 합을 구한다. -> 어떻게? 배열에 넣어서 합을 구함
+ * 2. x를 자릿수의 합으로 나눠 떨어지는지 확인한다. -> 나머지가 0인지 확인
+ * */
+
+package programmers;
+
+public class 하샤드수 {
+    public static void main(String[] args) {
+        Solution03 p = new Solution03();
+        System.out.println(p.solution(13));
+    }
+}
+
+class Solution03 {
+    public boolean solution(int x) {
+
+        String xToString = String.valueOf(x);
+
+        int sum = 0;
+        for (int i = 0; i < xToString.length(); i++) {
+            sum += Integer.parseInt(String.valueOf(xToString.charAt(i)));
+        }
+
+        return x % sum == 0 ? true : false;
+    }
+}
+/*
+* 회고
+* 언어는 자주 안 쓰면 어색해지는 거 같다. 근데 어제보단 자바가 익숙해졌다고 느낀다.
+* 어제보단 빠르게 풀었다.
+* 생각만 하는게 아니라 문제분석, 계획을 세워서 적고 정리하니까 알고리즘 푸는데 많이 도움된다.
+* */

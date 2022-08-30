c21ec637a0d68a1d5e4f3affa986cc26466f9f11
wisdom08 <wisdom08@kakao.com>
2022-08-31 04:46:16 +0900

    feat: 2022-08-31

--- algorithm/codingtest/java/_01_03.java
+++ algorithm/codingtest/java/_01_03.java
@@ -4,6 +4,25 @@ import java.util.Scanner;
 
 public class _01_03 {
 
+    public static String solution_02(String str) {
+
+        String result = "";
+        int max = Integer.MIN_VALUE, pos;
+
+        while ((pos = str.indexOf(" ")) != -1) {
+            String tmp = str.substring(0, pos);
+            int len = tmp.length();
+            if (len > max) {
+                max = len;
+                result = tmp;
+            }
+            str = str.substring(pos + 1);
+        }
+
+        if(str.length() > max) result = str;
+        return result;
+    }
+
     public static String solution(String str) {
 
         String[] split = str.split(" ");
@@ -25,6 +44,6 @@ public class _01_03 {
         Scanner in = new Scanner(System.in);
         String str = in.nextLine();
 
-        System.out.println(solution(str));
+        System.out.println(solution_02(str));
     }
 }

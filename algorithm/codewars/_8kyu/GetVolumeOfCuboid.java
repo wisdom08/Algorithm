c9af193c9b67a722d787e82db4075784e8d26a43
wisdom08 <wisdom08@kakao.com>
2022-10-24 02:13:27 +0900

    feat: 2022-10-23

---
+++ algorithm/codewars/_8kyu/GetVolumeOfCuboid.java
@@ -0,0 +1,11 @@
+package codewars._8kyu;
+
+public class GetVolumeOfCuboid {
+    public static double getVolumeOfCuboid(final double length, final double width, final double height) {
+        return length * width * height;
+    }
+}
+
+/*
+* https://www.codewars.com/kata/58261acb22be6e2ed800003a/train/java
+* */
---
+++ algorithm/codewars/_8kyu/Goals.java
@@ -0,0 +1,11 @@
+package codewars._8kyu;
+
+public class Goals {
+    public static int goals(int laLigaGoals, int copaDelReyGoals, int championsLeagueGoals) {
+        return championsLeagueGoals + copaDelReyGoals + laLigaGoals;
+    }
+}
+
+/*
+* https://www.codewars.com/kata/55f73be6e12baaa5900000d4/train/java
+* */

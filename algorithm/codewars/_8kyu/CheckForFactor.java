901b8ce1b91b38c2d3dcd026c3ff2b01e300e213
wisdom08 <wisdom08@kakao.com>
2022-10-24 02:08:53 +0900

    feat: 2022-10-22

---
+++ algorithm/codewars/_8kyu/CheckForFactor.java
@@ -0,0 +1,11 @@
+package codewars._8kyu;
+
+public class CheckForFactor {
+    public static boolean checkForFactor(int base, int factor) {
+        return base % factor == 0;
+    }
+}
+
+/*
+* https://www.codewars.com/kata/55cbc3586671f6aa070000fb/train/java
+* */
---
+++ algorithm/codewars/_8kyu/Correct.java
@@ -0,0 +1,11 @@
+package codewars._8kyu;
+
+public class Correct {
+    public static String correct(String string) {
+        return string.replace("5", "S").replace("0", "O").replace("1", "I");
+    }
+}
+
+/*
+* https://www.codewars.com/kata/577bd026df78c19bca0002c0/train/java
+* */

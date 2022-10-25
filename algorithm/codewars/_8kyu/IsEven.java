94cec83ec6517370bda25f06e4ecdb9de198e7e9
wisdom08 <wisdom08@kakao.com>
2022-10-25 23:59:14 +0900

    feat: 2022-10-24

---
+++ algorithm/codewars/_8kyu/IsEven.java
@@ -0,0 +1,11 @@
+package codewars._8kyu;
+
+public class IsEven {
+    public boolean isEven(double n) {
+        return n % 2 == 0;
+    }
+}
+
+/*
+* https://www.codewars.com/kata/555a67db74814aa4ee0001b5/train/java
+* */

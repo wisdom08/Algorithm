38fb6bc577ace0d6194b70bda6dd3b1f092d6570
wisdom08 <wisdom08@kakao.com>
2022-10-27 00:04:50 +0900

    feat: 2022-10-26

---
+++ algorithm/codewars/_8kyu/CountOddNumbersBelowN.java
@@ -0,0 +1,11 @@
+package codewars._8kyu;
+
+public class CountOddNumbersBelowN {
+    public static int oddCount(int n){
+        return n / 2;
+    }
+}
+
+/*
+* https://www.codewars.com/kata/59342039eb450e39970000a6/train/java
+* */
---
+++ algorithm/codewars/_8kyu/SwitchItUp.java
@@ -0,0 +1,31 @@
+package codewars._8kyu;
+
+public class SwitchItUp {
+    public static String switchItUp(int number) {
+        switch (number) {
+            case 0:
+                return "Zero";
+            case 1:
+                return "One";
+            case 2:
+                return "Two";
+            case 3:
+                return "Three";
+            case 4:
+                return "Four";
+            case 5:
+                return "Five";
+            case 6:
+                return "Six";
+            case 7:
+                return "Seven";
+            case 8:
+                return "Eight";
+        }
+        return "Nine";
+    }
+}
+
+/*
+* https://www.codewars.com/kata/5808dcb8f0ed42ae34000031/java
+* */

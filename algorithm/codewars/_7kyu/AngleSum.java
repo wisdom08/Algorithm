849e97615d9161fa10c0b3ba58c0f34d334eded2
wisdom08 <wisdom08@kakao.com>
2022-11-05 10:50:58 +0900

    feat: 2022-11-05

---
+++ algorithm/codewars/_7kyu/AngleSum.java
@@ -0,0 +1,8 @@
+package codewars._7kyu;
+
+public class AngleSum {
+    public static int sumOfAngles(int n) {
+        return (n - 2) * 180;
+    }
+}
+// https://www.codewars.com/kata/5a03b3f6a1c9040084001765/train/java

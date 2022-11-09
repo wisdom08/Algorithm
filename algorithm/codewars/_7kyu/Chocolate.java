3482290536053d35590339e41474f4cb9df1454e
wisdom08 <wisdom08@kakao.com>
2022-11-09 23:28:20 +0900

    feat: 2022-11-07

---
+++ algorithm/codewars/_7kyu/Chocolate.java
@@ -0,0 +1,9 @@
+package codewars._7kyu;
+
+public class Chocolate {
+    public static long breakChocolate(long n, long m) {
+        return n * m - 1;
+    }
+}
+
+// https://www.codewars.com/kata/534ea96ebb17181947000ada/train/java

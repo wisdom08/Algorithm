782d3c56118f37e0d9b79dcd767d90cac48df050
wisdom08 <wisdom08@kakao.com>
2022-11-12 09:28:05 +0900

    feat: 2022-11-12

---
+++ algorithm/codewars/_8kyu/Square.java
@@ -0,0 +1,12 @@
+package codewars._8kyu;
+
+public class Square {
+    public static int square(int n){
+        return (int) Math.pow(n, 2);
+    }
+    public static int square_2(int n){
+        return n*n;
+    }
+}
+
+// https://www.codewars.com/kata/523b623152af8a30c6000027/train/java

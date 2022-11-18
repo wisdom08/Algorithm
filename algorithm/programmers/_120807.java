7d9bfbe26c4752d14bc1d21ab7e8783fe307f558
wisdom08 <wisdom08@kakao.com>
2022-11-18 09:16:36 +0900

    feat: 2022-11-18

---
+++ algorithm/programmers/_120807.java
@@ -0,0 +1,9 @@
+package programmers;
+
+public class _120807 {
+    public int solution(int num1, int num2) {
+        return num1 == num2 ? 1 : -1;
+    }
+}
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/120807

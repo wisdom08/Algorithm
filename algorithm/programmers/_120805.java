f29382497eed3a630fd847d66e1ee38dcfb6bffa
wisdom08 <wisdom08@kakao.com>
2022-11-24 09:57:56 +0900

    feat: 2022-11-24

---
+++ algorithm/programmers/_120805.java
@@ -0,0 +1,9 @@
+package programmers;
+
+public class _120805 {
+    public int solution(int num1, int num2) {
+        return num1 / num2;
+    }
+}
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/120805

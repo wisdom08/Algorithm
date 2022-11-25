2a8f50ce740d410abbe85f8702b6002d6e231599
wisdom08 <wisdom08@kakao.com>
2022-11-25 12:49:34 +0900

    feat: 2022-11-25

---
+++ algorithm/programmers/_120806.java
@@ -0,0 +1,9 @@
+package programmers;
+
+public class _120806 {
+    public static int solution(int num1, int num2) {
+        return (int)((double) num1 / num2 * 1000);
+    }
+}
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/120806
---
+++ algorithm/programmers/_120808.java
@@ -0,0 +1,24 @@
+package programmers;
+
+public class _120808 {
+    public int[] solution(int denum1, int num1, int denum2, int num2) {
+        int[] answer = new int[2];
+        answer[0] = denum1 * num2 + denum2 * num1;
+        answer[1] = num1 * num2;
+        int min = Math.min(answer[0], answer[1]);
+        int max = 0;
+
+        for (int i = 1; i <= min; i++) {
+            if (answer[0] % i == 0 && answer[1] % i == 0) {
+                max = i;
+            }
+        }
+
+        answer[0] /= max;
+        answer[1] /= max;
+
+        return answer;
+    }
+}
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/120808

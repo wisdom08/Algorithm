c648076e823579a8e4578f56d668c6d02ca11a17
wisdom08 <wisdom08@kakao.com>
2022-12-15 01:29:15 +0900

    feat: 2022-12-15

---
+++ algorithm/programmers/_120898.java
@@ -0,0 +1,9 @@
+package programmers;
+
+public class _120898 {
+    public int solution(String message) {
+        return message.length()*2;
+    }
+}
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/120898

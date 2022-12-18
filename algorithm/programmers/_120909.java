506db116f75b40e9862f44b471aaac0a1ea68912
wisdom08 <wisdom08@kakao.com>
2022-12-18 10:47:02 +0900

    feat: 2022-12-18

---
+++ algorithm/programmers/_120909.java
@@ -0,0 +1,9 @@
+package programmers;
+
+public class _120909 {
+    public int solution(int n) {
+        return n % Math.sqrt(n)  == 0 ? 1 : 2;
+    }
+}
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/120909

ced4077bdf3120cb1211aef1853bef92909186c2
wisdom08 <wisdom08@kakao.com>
2023-01-03 23:01:55 +0900

    feat: 2023-01-02

---
+++ algorithm/src/main/java/practice/programmers/_120845.java
@@ -0,0 +1,9 @@
+package practice.programmers;
+
+public class _120845 {
+    public int solution(int[] box, int n) {
+        return (box[0]/n)*(box[1]/n)*(box[2]/n);
+    }
+}
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/120845

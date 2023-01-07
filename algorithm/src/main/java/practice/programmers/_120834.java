0f4a3727384011244c215935855f3105f4fdf0bf
wisdom08 <wisdom08@kakao.com>
2023-01-07 09:42:55 +0900

    fix: 주석 제거

--- algorithm/src/main/java/practice/programmers/_120834.java
+++ algorithm/src/main/java/practice/programmers/_120834.java
@@ -5,13 +5,6 @@ import org.junit.jupiter.api.Test;
 import java.util.HashMap;
 import java.util.Map;
 
-//  a는 0, b는 1, c는 2, ..., j는 9
-
-/*
- * 맵에 a,~j 를 숫자 0~9에 맵핑해서 넣는다.
- * 주어진 파라미터 값이 맵에서 어떤 값과 일치하는지 체크한다.
- * stringbuild 자료 구조에 하나 씩 더해나간다.
- * */
 public class _120834 {
     public String solution(int age) {
 

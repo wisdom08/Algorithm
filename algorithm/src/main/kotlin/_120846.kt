c853d3b87321730265d7eed940b5c2de63a8ba7c
wisdom08 <wisdom08@kakao.com>
2023-01-12 22:21:47 +0900

    feat: 2023-01-12

---
+++ algorithm/src/main/kotlin/_120846.kt
@@ -0,0 +1,26 @@
+package practice.programmers
+
+class _120846 {
+    fun div(n: Int): Int {
+        var count = 0
+        for (i in 1 .. n) {
+            if (n % i == 0) count++
+        }
+
+        return count
+    }
+
+    fun solution(n: Int): Int {
+        var answer = 0
+        for (i in 1 .. n) {
+            if (div(i) != 2 && div(i) != 1) {
+                answer++
+            }
+        }
+        return answer
+    }
+
+    fun solution_2(n: Int) = (1..n).filter { i -> (1..i).filter { i % it == 0 }.size > 2 }.size
+}
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/120846?language=kotlin
